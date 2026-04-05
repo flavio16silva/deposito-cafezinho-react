61239f3a431c1bbd432a350a59a6bd8c3db08917
Flávio Silva <fal_silva@hotmail.com>
2026-04-04 22:29:45 -0300

    Data : Inserção catalogo de produtos: salgadinhos e combos

---
+++ src/data/combos.js
@@ -0,0 +1,37 @@
+export const combos = [
+  {
+    id: 6,
+    nome: "Pastel + 6 Heineken",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 7,
+    nome: "Bolinha queijo + 6 Heineken",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 8,
+    nome: "Coxinha + 6 Heineken",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 9,
+    nome: "Kibe + 6 Heineken",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 10,
+    nome: "Sortidos + 6 Heineken",
+    preco10: 80.00,
+    preco20: 95.00,
+    preco30: 110.00
+  }
+]
---
+++ src/data/salgadinhos.js
@@ -0,0 +1,37 @@
+export const salgadinhos = [
+  {
+    id: 1,
+    nome: "Pastel de carne ou pizza",
+    preco10: 18.00,
+    preco20: 36.00,
+    preco30: 48.00
+  },
+  {
+    id: 2,
+    nome: "Bolinha de queijo",
+    preco10: 18.00,
+    preco20: 36.00,
+    preco30: 48.00
+  },
+  {
+    id: 3,
+    nome: "Coxinha de frango",
+    preco10: 18.00,
+    preco20: 36.00,
+    preco30: 48.00
+  },
+  {
+    id: 4,
+    nome: "Kibe com queijo",
+    preco10: 18.00,
+    preco20: 36.00,
+    preco30: 48.00
+  },
+  {
+    id: 5,
+    nome: "Salgadinhos sortidos",
+    preco10: 20.00,
+    preco20: 38.00,
+    preco30: 50.00
+  }
+]
