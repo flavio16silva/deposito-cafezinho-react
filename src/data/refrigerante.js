72d27e934d22d83fd2a8f7a4272fb1e011af88b0
Flávio Silva <fal_silva@hotmail.com>
2026-04-07 16:33:30 -0300

    Data: Arrayscervejas e refrigerante criados

---
+++ src/data/refrigerante.js
@@ -0,0 +1,26 @@
+export const refri = [
+  {
+    id: 1,
+    nome: "Guaraná Antártica 1L",
+    preco: 8.00,
+    unidade: "un"
+  },
+  {
+    id: 2,
+    nome: "Coca Cola 1L",
+    preco: 10.00,
+    unidade: "un"
+  },
+  {
+    id: 3,
+    nome: "Pepsi 1L",
+    preco: 8.00,
+    unidade: "un"
+  },
+  {
+    id: 4,
+    nome: "Goob 1L",
+    preco: 7.00,
+    unidade: "un"
+  }
+]
