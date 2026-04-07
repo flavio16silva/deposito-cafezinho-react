d6e904c69c1a1ed14853b5381915ede63b925721
Flávio Silva <fal_silva@hotmail.com>
2026-04-07 07:41:04 -0300

    Data: Alteração array salgados: + salgados

--- src/data/salgadinhos.js
+++ src/data/salgadinhos.js
@@ -1,37 +1,44 @@
 export const salgadinhos = [
   {
     id: 1,
-    nome: "Pastel de carne ou pizza",
+    nome: "Pastel de carne",
     preco10: 18.00,
     preco20: 36.00,
     preco30: 48.00
   },
   {
     id: 2,
-    nome: "Bolinha de queijo",
+    nome: "Pastel de pizza",
     preco10: 18.00,
     preco20: 36.00,
     preco30: 48.00
   },
   {
     id: 3,
-    nome: "Coxinha de frango",
+    nome: "Bolinha de queijo",
     preco10: 18.00,
     preco20: 36.00,
     preco30: 48.00
   },
   {
     id: 4,
-    nome: "Kibe com queijo",
+    nome: "Coxinha de frango",
     preco10: 18.00,
     preco20: 36.00,
     preco30: 48.00
   },
   {
     id: 5,
-    nome: "Salgadinhos sortidos",
+    nome: "Kibe com queijo",
     preco10: 20.00,
     preco20: 38.00,
     preco30: 50.00
+  },
+  {
+    id: 6,
+    nome: "Salgadinhos sortidos",
+    preco10: 18.00,
+    preco20: 36.00,
+    preco30: 48.00
   }
 ]
