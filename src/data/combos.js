5e9ae131da22e17d40e72a998956d2bcddab23cc
Flávio Silva <fal_silva@hotmail.com>
2026-04-07 14:17:00 -0300

    Data: Combo adicionado

--- src/data/combos.js
+++ src/data/combos.js
@@ -1,35 +1,42 @@
 export const combos = [
   {
+    id: 1,
+    nome: "Pastel de carne + 6 Heineken",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 2,
+    nome: "Bolinha queijo + 6 Amstel Ultra",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 3,
+    nome: "Coxinha + 6 Stella Artois",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 4,
+    nome: "Kibe + 6 Budweiser",
+    preco10: 75.00,
+    preco20: 90.00,
+    preco30: 105.00
+  },
+  {
+    id: 5,
+    nome: "Sortidos + 6 Império Ultra",
+    preco10: 80.00,
+    preco20: 95.00,
+    preco30: 110.00
+  },
+  {
     id: 6,
-    nome: "Pastel + 6 Heineken",
-    preco10: 75.00,
-    preco20: 90.00,
-    preco30: 105.00
-  },
-  {
-    id: 7,
-    nome: "Bolinha queijo + 6 Heineken",
-    preco10: 75.00,
-    preco20: 90.00,
-    preco30: 105.00
-  },
-  {
-    id: 8,
-    nome: "Coxinha + 6 Heineken",
-    preco10: 75.00,
-    preco20: 90.00,
-    preco30: 105.00
-  },
-  {
-    id: 9,
-    nome: "Kibe + 6 Heineken",
-    preco10: 75.00,
-    preco20: 90.00,
-    preco30: 105.00
-  },
-  {
-    id: 10,
-    nome: "Sortidos + 6 Heineken",
+    nome: "Pastel de Pizza + 6 Heineken",
     preco10: 80.00,
     preco20: 95.00,
     preco30: 110.00
