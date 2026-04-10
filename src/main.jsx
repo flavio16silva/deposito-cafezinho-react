7e575d0535372f376fe0564cf1e15af67f4fb0bb
Flávio Silva <fal_silva@hotmail.com>
2026-04-09 23:37:27 -0300

    Main: Inserido Carrinho Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App.jsx"
+import { CarrinhoProvider } from "./context/carrinhoProvider.jsx"
 import "./index.css"
 
 
@@ -9,6 +10,8 @@ const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <App />
+    <CarrinhoProvider>
+      <App />
+    </CarrinhoProvider>
   </StrictMode>
 )
