9d749b2b6613f10538de347d09ceb04b4a7958c2
Flávio Silva <fal_silva@hotmail.com>
2026-04-09 23:39:49 -0300

    Data: Alteração do Id

--- src/data/combos.js
+++ src/data/combos.js
@@ -1,41 +1,41 @@
 export const combos = [
   {
-    id: 1,
+    id: 6,
     nome: "Pastel de carne + 6 Heineken",
     preco10: 75.00,
     preco20: 90.00,
     preco30: 105.00
   },
   {
-    id: 2,
+    id: 7,
     nome: "Bolinha queijo + 6 Amstel Ultra",
     preco10: 75.00,
     preco20: 90.00,
     preco30: 105.00
   },
   {
-    id: 3,
+    id: 8,
     nome: "Coxinha + 6 Stella Artois",
     preco10: 75.00,
     preco20: 90.00,
     preco30: 105.00
   },
   {
-    id: 4,
+    id: 9,
     nome: "Kibe + 6 Budweiser",
     preco10: 75.00,
     preco20: 90.00,
     preco30: 105.00
   },
   {
-    id: 5,
+    id: 10,
     nome: "Sortidos + 6 Império Ultra",
     preco10: 80.00,
     preco20: 95.00,
     preco30: 110.00
   },
   {
-    id: 6,
+    id: 11,
     nome: "Pastel de Pizza + 6 Heineken",
     preco10: 80.00,
     preco20: 95.00,
--- src/data/refrigerante.js
+++ src/data/refrigerante.js
@@ -1,24 +1,24 @@
 export const refri = [
   {
-    id: 1,
+    id: 12,
     nome: "Guaraná Antártica 1L",
     preco: 8.00,
     unidade: "un"
   },
   {
-    id: 2,
+    id: 13,
     nome: "Coca Cola 1L",
     preco: 10.00,
     unidade: "un"
   },
   {
-    id: 3,
+    id: 14,
     nome: "Pepsi 1L",
     preco: 8.00,
     unidade: "un"
   },
   {
-    id: 4,
+    id: 15,
     nome: "Goob 1L",
     preco: 7.00,
     unidade: "un"
