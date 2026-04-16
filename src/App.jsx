1238b59c962d86a4a5644d98f3d6949d2be706dd
Flávio Silva <fal_silva@hotmail.com>
2026-04-15 21:25:22 -0300

    App: Criaçãode nova rota cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import { Combos } from './components/combo'
 import { Bebidas } from './components/bebidas'
 import { Footer } from './components/footer'
 import { MeusPedidos } from './components/meusPedidos'
+import { Cadastro } from './components/cadastro'
 
 const Layout = () => {
   return (
@@ -48,6 +49,7 @@ const App = () => {
         <Route path="/combos" element={<Combos />} />
         <Route path="/bebidas" element={<Bebidas />} />
         <Route path="/meusPedidos" element={<MeusPedidos />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Route>
     )
   )
