323e2324b1c02adc58c64c41a53ecaaa0a000bf7
Flávio Silva <fal_silva@hotmail.com>
2026-04-23 20:28:37 -0300

    Cerveja: correção para cerveja.js

---
+++ src/data/cervejas.js
@@ -0,0 +1,32 @@
+export const cervejas = [
+  {
+    id: 1,
+    nome: "Amstel Ultra",
+    preco: 9.00,
+    unidade: "un"
+  },
+  {
+    id: 2,
+    nome: "Heineken",
+    preco: 10.00,
+    unidade: "un"
+  },
+  {
+    id: 3,
+    nome: "Stella Artois",
+    preco: 10.00,
+    unidade: "un"
+  },
+  {
+    id: 4,
+    nome: "Budweiser",
+    preco: 9.00,
+    unidade: "un"
+  },
+  {
+    id: 5,
+    nome: "Império Ultra",
+    preco: 8.00,
+    unidade: "un"
+  }
+]
