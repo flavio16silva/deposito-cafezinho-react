3c8e1bae85ab81354749f2530613c23b55709b5b
Flávio Silva <fal_silva@hotmail.com>
2026-05-25 07:36:20 -0300

    Admin.js: criação da router.put -> atualizar status do pedido

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -42,4 +42,46 @@ router.get('/pedidos', async (req, res) => {
   }
 })
 
+// Rota para atualizar o status de um pedido
+// URL final: PUT /api/admin/pedidos/:id/status
+router.put('/pedidos/:id/status', async (req, res) => {
+  try {
+    //Extrair o id do pedido da URL
+    const pedidoId = req.params.id
+
+    //Extrair o novo status do corpo da requisição
+    const { status } = req.body
+
+    // //Validar se o status foi informado
+    // if (!status) {
+    //   return res.status(400).json({ erro: 'Status é obrigatório' })
+    // }
+
+    //Verificar se o pedido existe
+    const [pedido] = await db.query(
+      'SELECT id FROM pedidos WHERE id = ?',
+      [pedidoId]
+    )
+
+    if (pedido.length === 0) {
+      return res.status(400).json({ erro: 'Pedido não encontrado ' })
+    }
+
+    //Atualizar o status no banco
+    await db.query(
+      'UPDATE pedidos SET status = ? WHERE id = ?',
+      [status, pedidoId]
+    )
+
+    //Retornar sucesso
+    res.status(200).json({
+      mensagem: `Status do pedido ${pedidoId} atualizado para ${status}`
+    })
+
+  } catch (error) {
+    console.error('❌ Erro ao atualizar status:', error)
+    res.status(500).json({ erro: 'Erro interno do servidor' })
+  }
+})
+
 module.exports = router
